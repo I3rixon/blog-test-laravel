855d758562101566ad190515db25e61aeded7a51
Alexander Z. <lulzsecer@gmail.com>
2025-01-18 10:14:37 +0200

    routes updates with comments

--- routes/web.php
+++ routes/web.php
@@ -4,8 +4,13 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\CommentController;
 
+// Route for the home page
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Resource routes for posts
 Route::resource('posts', PostController::class);
+
+// Route to store comments for a specific post
 Route::post('posts/{post}/comments', [CommentController::class, 'store'])->name('comments.store');
